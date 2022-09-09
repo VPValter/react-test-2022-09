52bb13e3748fe101ac7cabe3c670e8c55092645c
Vladimir Perić <mgr.art.v.peric@gmail.com>
2022-09-10 00:30:18 +0200

    Initial data fetching test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [results, setResults] = useState([]);
+
+  useEffect(() => {
+    fetch('https://frontend-api-test-nultien.azurewebsites.net/api/BlogPosts')
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          console.log(result);
+          setResults(result);
+        },
+        // (from React docs):
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          console.log(error);
+        }
+      );
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <p>hello it's-a me, new App component</p>
+      {results.resultData && results.resultData.length ? (
+        results.resultData.map((item) => (
+          <div key={item.id}>
+            <h3>{item.title}</h3>
+            <small>{item.text}</small>
+          </div>
+        ))
+      ) : (
+        <p>No posts found</p>
+      )}
     </div>
   );
 }
