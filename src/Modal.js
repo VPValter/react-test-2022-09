76a5ca81d78bb606c33c56c86de53f7122354747
Vladimir Perić <mgr.art.v.peric@gmail.com>
2022-09-11 12:10:54 +0200

    First post request test

--- src/Modal.js
+++ src/Modal.js
@@ -1,4 +1,29 @@
+import { useState } from 'react';
+
 const Modal = ({ closeModal }) => {
+  const [title, setTitle] = useState('');
+  const [text, setText] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const formData = { title, text };
+    const options = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(formData),
+    };
+    fetch(
+      'https://frontend-api-test-nultien.azurewebsites.net/api/BlogPosts',
+      options
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        // TODO: CLOSE MODAL ( or show a message in it and close it manually ? )
+        // TODO: REFRESH THE BLOGLIST
+      });
+  };
+
   return (
     <div className='modal close-modal' onClick={closeModal}>
       <div className='modal-content'>
@@ -9,7 +34,7 @@ const Modal = ({ closeModal }) => {
           </button>
         </div>
         <div className='modal-body'>
-          <form>
+          <form onSubmit={handleSubmit}>
             <div className='form-group'>
               <label htmlFor='title'>Title</label>
               <input
@@ -17,20 +42,24 @@ const Modal = ({ closeModal }) => {
                 id='title'
                 required
                 placeholder='Title of the post'
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
               />
             </div>
             <div className='form-group'>
-              <label htmlFor='postText'>Text</label>
+              <label htmlFor='text'>Text</label>
               <textarea
-                name='postText'
-                id='postText'
+                name='text'
+                id='text'
                 rows='8'
                 required
                 placeholder='Text of the post'
+                value={text}
+                onChange={(e) => setText(e.target.value)}
               ></textarea>
             </div>
             <div className='buttons-group'>
-              <button type='button'>Post</button>
+              <button>Post</button>
               <button type='button' className='close-modal'>
                 Cancel
               </button>
